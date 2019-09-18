4614a6d990c74e8a6ebb002b1493df870df7e34b
wangfh <315999946@qq.com>
2019-09-18 20:27:02 +0800

    Leetcode

--- stage-13-leetCode/509-斐波那契数.js
+++ stage-13-leetCode/509-斐波那契数.js
@@ -1,3 +1,35 @@
+/*
+* https://leetcode-cn.com/problems/fibonacci-number/
+*
+* 斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
+
+F(0) = 0,   F(1) = 1
+F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+给定 N，计算 F(N)。
+
+==========示例 1：
+
+输入：2
+输出：1
+解释：F(2) = F(1) + F(0) = 1 + 0 = 1.
+
+==========示例 2：
+
+输入：3
+输出：2
+解释：F(3) = F(2) + F(1) = 1 + 1 = 2.
+
+==========示例 3：
+
+输入：4
+输出：3
+解释：F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+提示：
+0 ≤ N ≤ 30
+*
+* */
+
 //0 1 1 2 3 5
 function fib1(n) {
     if (n <= 1) return n;
@@ -10,7 +42,7 @@ function fib1(n) {
 *
 * */
 function fib2(n) {
-    if (n <= 1) return 1;
+    if (n <= 1) return n;
     let first = 0;
     let second = 1;
     /*分析上边的 值, 当n传2 的时候 需要加1次  0+1=1
@@ -33,4 +65,4 @@ console.log(fib1(19));
 console.log(fib1(30));
 // console.log(fib(64)); //第一种算法,到64就执行不动了,耗费大量时间
 
-console.log(fib2(90));
+console.log(fib2(64));
