74d2594d2f5e3ce4c3578ded83000248d9b71208
Ne <315999946@qq.com>
2020-04-25 21:10:37 +0800

    二叉树

--- stage-12-数据结构与算法/04-二叉搜索树/01-二叉搜索树.ts
+++ stage-12-数据结构与算法/04-二叉搜索树/01-二叉搜索树.ts
@@ -1,6 +1,13 @@
 import {Person} from './Person'
 import {Comparator} from './Comparator'
 
+// MARK: ----- 访问器 -----
+//当遍历树的时候,每遍历到一个元素,该如何处理该元素由这个接口往外抛出
+interface Visitor<E> {
+    visit(element : E) : void;
+}
+
+// MARK: ----- 节点 -----
 class Node<E> {
     element : E;
     right : Node<E> = null;
@@ -10,12 +17,23 @@ class Node<E> {
         this.element = element;
         this.parent = parent;
     }
+
+    // 判断是不是叶子
+    isLeaf() : boolean {
+        return this.left === null && this.right === null;
+    }
+
+    //是否拥有两个子节点
+    hasTwoChildren() : boolean {
+        return this.left !== null && this.right !== null;
+    }
 }
 
 class BinarySearchTree<E> {
     private count = 0;
     private root : Node<E> = null;
     private comparator : Comparator<E>
+    private sb : string = "";
 
     constructor(comparator : Comparator<E>) {
         this.comparator = comparator;
@@ -88,7 +106,6 @@ class BinarySearchTree<E> {
     preorderTraversal() : void {
         this.preorderTraversals(this.root);
     }
-
     private preorderTraversals(node : Node<E>) {
         if (node === null) return;
         console.log(node.element);
@@ -100,7 +117,6 @@ class BinarySearchTree<E> {
     inorderTraversal() : void {
         this.inorderTraversals(this.root);
     }
-
     private inorderTraversals(node : Node<E>) {
         if (node === null) return;
         this.inorderTraversals(node.left);
@@ -112,7 +128,6 @@ class BinarySearchTree<E> {
     postorderTraversal() : void {
         this.postTraversals(this.root);
     }
-
     private postTraversals(node : Node<E>) {
         if (node === null) return;
         this.postTraversals(node.left);
@@ -140,6 +155,201 @@ class BinarySearchTree<E> {
         }
     }
 
+    // MARK: ----- 每遍历到一个节点,把该节点抛出去,由外部实现得到节点后要执行的操作 -----
+    //层序遍历,
+    levelOrder(visitor : Visitor<E>) : void {
+        if (this.root === null) return
+
+        let queue = new Array<Node<E>>();
+        queue.push(this.root); //头部入队
+
+        while (queue.length > 0) {
+            let node : Node<E> = queue.shift(); //出队
+
+            visitor.visit(node.element);
+
+            if (node.left !== null) {
+                queue.push(node.left);
+            }
+
+            if (node.right !== null) {
+                queue.push(node.right)
+            }            
+        }
+    }
+
+    //前序遍历
+    preOrder(visitor : Visitor<E>) : void {
+        this.preOrders(this.root,visitor);
+    }
+    private preOrders(node : Node<E> ,visitor : Visitor<E>) : void {
+        if (node === null || visitor === null) return;
+        visitor.visit(node.element);
+        this.preOrders(node.left,visitor);
+        this.preOrders(node.right,visitor);
+    }
+
+    //中序遍历
+    inOrder(visitor : Visitor<E>) : void {
+        this.inOrders(this.root,visitor);
+    }
+    private inOrders(node : Node<E> ,visitor : Visitor<E>) : void {
+        if (node === null || visitor === null) return;
+        this.inOrders(node.left,visitor);
+        visitor.visit(node.element);
+        this.inOrders(node.right,visitor);
+    }
+
+    //后序遍历
+    postOrder(visitor : Visitor<E>) : void {
+        this.postOrders(this.root,visitor);
+    }
+    private postOrders(node : Node<E> ,visitor : Visitor<E>) : void {
+        if (node === null || visitor === null) return;
+        this.postOrders(node.left,visitor);
+        this.postOrders(node.right,visitor);
+        visitor.visit(node.element);
+    }
+
+    // MARK: ----- 打印树的结构 -----
+    toString() : string {
+        this.toStrings(this.root,this.sb,"");
+        return this.sb;
+    }
+    private toStrings(node : Node<E>,sb : string,prefix : string) : void {
+        if (node === null) return;
+        this.sb = this.sb + prefix + node.element + '\n'; 
+        this.toStrings(node.left,this.sb, prefix + "L---");
+        this.toStrings(node.right,this.sb, prefix + "R---");
+    }
+
+    // MARK: ----- 树的高度 -----
+    //对于整个树而言,树的高度就是根节点的高度
+    //递归实现:
+    height() : number {
+        return this.heigthWithNode(this.root);
+    }
+    //获取某个节点的高度
+    private heigthWithNode(node : Node<E>) : number {
+        if (node === null) return 0;
+        return 1+ Math.max(this.heigthWithNode(node.right),this.heigthWithNode(node.left));
+    }
+
+    //迭代实现:(层序遍历的思想)
+    heightIteration() : number {
+        if (this.root === null) return 0;
+
+        let height = 0;
+        let levelSize = 1; //存储每一层元素的数量 1 表示第一层根节点的数量
+
+        let queue = new Array<Node<E>>();
+        queue.push(this.root); //头部入队
+
+        while (queue.length > 0) {
+            let node : Node<E> = queue.shift(); //出队
+
+            levelSize--;
+
+            if (node.left !== null) {
+                queue.push(node.left);
+            }
+
+            if (node.right !== null) {
+                queue.push(node.right)
+            }  
+
+            if (levelSize === 0) { //意味着即将访问下一次
+                levelSize = queue.length;
+                height++;
+            }
+        }
+        return height;
+    }
+
+    // MARK: ----- 判断是否是完全二叉树 -----
+    //完全二叉树的特点: 叶子节点只会出现在最后两层,并且最后一层叶子靠左对齐
+    /*
+        其实就是个排列组合问题 :
+        左 不为空  右 不为空  正常的入队
+        左 null   右 不为空  false
+        左 不为空  右 null   后边的节点 左右都应该是 null
+        左 null   右 null   后边的节点 左右都应该是 null
+    */
+    isComplete() : boolean {
+        if (null === this.root) return false;
+
+        let queue = new Array<Node<E>>();
+        queue.push(this.root); //头部入队
+
+        let leaf = false
+        while (queue.length > 0) {
+            let node : Node<E> = queue.shift(); //出队
+
+            if (leaf && !node.isLeaf()) { // 也就是后边必须是叶子节点的情况下,遍历出了一个不是叶子节点的情况,直接返回false,说明不是完全二叉树
+                return false
+            }
+
+            if (node.hasTwoChildren()) {
+                queue.push(node.left);
+                queue.push(node.right)
+            } else if (node.left === null && node.right !== null) {
+                return false;
+            } else { //后面遍历的节点都必须是叶子节点,才会是完全二叉树
+                leaf = true;
+            }
+        }
+        return true;
+    }
+
+    // MARK: ----- 前驱节点 -----
+    /*
+    1.如果一个节点的 左子树 != null,就在左子树中一直往右找
+    2.如果一个节点的 左子树 == null, parent != null 就顺着 parent 一直往上找 直到 是 父节点的右子树 为止
+    3.如果一个节点的 左子树 == null, parent == null 没有前驱
+    */
+    private predecessor(node : Node<E>) {
+        if (node === null) return null;
+
+        // 前驱节点在左子树中,(left.right.right.right.....)
+        let p = node.left
+        if (p != null) {
+            while (p.right !== null) {
+                p = p.right;
+            }
+            return p;
+        }
+
+        //从父节点,祖父节点中寻找
+        while (node.parent !== null && node === node.parent.left) {
+            node = node.parent;
+        }
+        //能来的这里,就有两种情况
+        // 1.node.parent === null
+        // 2.node = node.parent.right;
+        return node.parent;
+    }
+    // MARK: ----- 后继节点 -----
+    private successor(node : Node<E>) {
+        if (node === null) return null;
+
+        // 后继节点在右子树中,(right.left.left.left.....)
+        let s = node.right
+        if (s != null) {
+            while (s.left !== null) {
+                s = s.left;
+            }
+            return s;
+        }
+
+        //从父节点,祖父节点中寻找
+        while (node.parent !== null && node === node.parent.right) {
+            node = node.parent;
+        }
+        //能来的这里,就有两种情况
+        // 1.node.parent === null
+        // 2.node = node.parent.left;
+        return node.parent;
+    }
 
     // MARK: ----- 节点比较的规则 -----
     private compare(e1 : E,e2 : E) : number {
@@ -155,8 +365,8 @@ class BinarySearchTree<E> {
     }
 }
 
+//测试树的遍历
 function test1() : void {
-    //创建比较器,让person按照年龄大的就算大排序
     class NumberCompator implements Comparator<number> {
         compare(e1: number, e2: number): number {
             return e1 - e2;
@@ -175,8 +385,25 @@ function test1() : void {
     bst.postorderTraversal();
     console.log('------');
     bst.levelorderTraversal();
+
+    
+    //创建访问器,自定义访问的规则
+    class VistorClass implements Visitor<number> {
+        visit(element: number): void {
+            console.log("_" + element + "_");
+        } 
+    }
+   
+    bst.preOrder(new VistorClass());
+    console.log('------');
+    bst.inOrder(new VistorClass());
+    console.log('------');
+    bst.postOrder(new VistorClass());
+    console.log('------');
+    bst.levelOrder(new VistorClass());
 }
 
+//测试比较器的使用
 function test2() : void {
     //创建比较器,让person按照年龄大的就算大排序
     class PersonComparator implements Comparator<Person> {
@@ -190,7 +417,7 @@ function test2() : void {
     bst1.add(new Person(21));
 }
 
-
+//测试比较器的使用
 function test3() : void {
     //创建比较器,让person按照年龄大的就算小排序
     class PersonComparator1 implements Comparator<Person> {
@@ -205,6 +432,41 @@ function test3() : void {
     bst2.preorderTraversal();
 }
 
-test1();
+//测试树的高度
+function test4() : void {
+    class NumberCompator implements Comparator<number> {
+        compare(e1: number, e2: number): number {
+            return e1 - e2;
+        }
+    }
+
+    let bst = new BinarySearchTree<number>(new NumberCompator());
+    for (let i = 0; i < 15; i++) {
+        bst.add(Math.round(Math.random() * 100))
+    }
+    console.log( bst.toString());
+    console.log(bst.height());
+    console.log(bst.heightIteration());
+}
+
+//测试是否是完成二叉树
+function test5() : void {
+    class NumberCompator implements Comparator<number> {
+        compare(e1: number, e2: number): number {
+            return e1 - e2;
+        }
+    }
+
+    let arr = [7,4,9,2,5];
+    let bst = new BinarySearchTree<number>(new NumberCompator());
+    for (let i = 0; i < arr.length; i++) {
+        bst.add(arr[i])
+    }
+
+    console.log( bst.toString());
+    console.log(bst.isComplete());
+}
+
+test5();
 
 export {BinarySearchTree}
